ad55253fa65cb59d6201bef146ea255696e9f0a7
Bryant Tang <96232475+Bryant-Tang@users.noreply.github.com>
2023-06-07 12:53:42 +0800

    revision

--- src/App.java
+++ src/App.java
@@ -1,51 +1,56 @@
 import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-
+import javax.swing.WindowConstants;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.util.ArrayList;
 
 public class App {
-    static Mode mode;
-    static UMLMenuBarController UMLMenuBar;
-    static UMLButtonPaneController UMLButtonPane;
-    static UMLCanvasPaneController UMLCanvasPane;
-    static ArrayList<BasicObject> selectComponent = new ArrayList<>();
-    static ArrayList<CompositeObject> selectGroup = new ArrayList<>();
+    static String appTitle = "UML Editor";
+    static Dimension canvasPreferredSize = new Dimension(400, 400);
 
-    private static void createApp() {
-        JFrame mainFrame = new JFrame("UML editor");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        mode = new Mode(Mode.SELECT);
-
-        JMenuBar menuBar = new JMenuBar();
-        mainFrame.setJMenuBar(menuBar);
-        UMLMenuBar = new UMLMenuBarController(mainFrame, menuBar, selectComponent, selectGroup);
-
-        JPanel contentPane = new JPanel(new BorderLayout(5, 5));
-        mainFrame.setContentPane(contentPane);
-
-        JPanel buttonPane = new JPanel(new BorderLayout(5, 5));
-        contentPane.add(buttonPane, BorderLayout.LINE_START);
-        UMLButtonPane = new UMLButtonPaneController(buttonPane, mode);
-
-        JLayeredPane canvasPane = new JLayeredPane();
-        contentPane.add(canvasPane, BorderLayout.CENTER);
-        UMLCanvasPane = new UMLCanvasPaneController(canvasPane, mode, selectComponent, selectGroup);
-
-        mainFrame.setSize(new Dimension(960, 640));
-        mainFrame.setMinimumSize(new Dimension(640, 640));
+    public static void main(String[] args) {
+        JFrame mainFrame = createMainFrame();
+        addMenu(mainFrame);
+        addButtonPane(mainFrame);
+        addCanvasPane(mainFrame);
+        mainFrame.pack();
         mainFrame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createApp();
-            }
-        });
+    static JFrame createMainFrame() {
+        JFrame mainFrame = new JFrame(appTitle);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return mainFrame;
+    }
+
+    static void addMenu(JFrame mainFrame) {
+        JMenuItem renameMenuItem = new JMenuItem("rename");
+        JMenuItem groupMenuItem = new JMenuItem("group");
+        JMenuItem ungroupMenuItem = new JMenuItem("ungroup");
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenuBar menuBar = new JMenuBar();
+        editMenu.add(renameMenuItem);
+        editMenu.add(groupMenuItem);
+        editMenu.add(ungroupMenuItem);
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        mainFrame.setJMenuBar(menuBar);
+    }
+
+    static void addButtonPane(JFrame mainFrame) {
+        JPanel buttonPane = new JPanel();
+        mainFrame.add(buttonPane, BorderLayout.LINE_START);
+    }
+
+    static void addCanvasPane(JFrame mainFrame) {
+        JPanel canvas = new JPanel();
+        canvas.setPreferredSize(canvasPreferredSize);
+        canvas.setBackground(Color.WHITE);
+        mainFrame.add(canvas, BorderLayout.CENTER);
     }
 }
